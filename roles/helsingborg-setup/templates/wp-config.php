0ca19d9d23435c70b876858f6ccaf545980a22e3
ownbite <linuswallin87@gmail.com>
2015-02-25 15:40:11 +0100

    Add domain name.

--- roles/helsingborg-setup/templates/wp-config.php
+++ roles/helsingborg-setup/templates/wp-config.php
@@ -77,7 +77,7 @@ define('FS_METHOD', 'direct');
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', '{{ ansible_enp0s8.ipv4.address }}');
+define('DOMAIN_CURRENT_SITE', '{{ domain_name }}');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
